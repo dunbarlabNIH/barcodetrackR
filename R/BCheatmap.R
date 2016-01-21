7bb4da949494b8429ea9d4fe3ee572d331305565
d93espinoza <d93espinoza@gmail.com>
2016-01-21 14:14:27 -0500

    Continued development of BCheatmap

--- R/BCheatmap.R
+++ R/BCheatmap.R
@@ -1,6 +1,6 @@
-#'@title BCheatmap
+#'@title BCheatmap (Barcode Heatmap)
 #'
-#'@description Creates a heatmap using the top 'n' rows from each column. Data undergoes a natural log transformation before being plotted. Optionally, will write a percentage key to the current directory.
+#'@description Creates a heatmap using the top 'n' rows from each column. More or less a wrapper for gplots' "heatmap.2" function :)
 #'
 #'@param your_data A data frame. Usually individual barcodes in rows and samples in columns.
 #'@param names Names for each column of the data frame.
@@ -9,28 +9,32 @@
 #'@param grid Logical. Include a grid or not in the heatmap.
 #'@param columnLabels The size of the column labels.
 #'@param dendro Whether or not to rearrange data with dendrogram.
+#'@param star_size The numerical size of the cell note labels.
 #'@param printtable Logical. Prints percent contribution as a table instead of plotting it.
-#'@param log_transorm Logical. Log transform the data or not.
-#'@param log_choice Data is log transformed with this log
+#'@param table_option Character. One of "logs", "reads", or "percents".
+#'@param log_transform Logical. Log transform data before clustering and plotting.
+#'@param log_choice Data is log transformed with this log.
 #'@param distance_method Character. One of "euclidean", "minkowski", "canberra", manhatan", "maximum", or "binary"
-#'@param minkowski_power The power of the Minkowski distance
-#'@param cellnote_option Character. One of "stars" or "percents"
+#'@param minkowski_power The power of the Minkowski distance (if minkowski is used).
+#'@param cellnote_option Character. One of "stars", "reads", or "percents"
 #'@param hclust_linkage Character. One of one of "ward.D", "ward.D2", "single", "complete", "average" (= UPGMA), "mcquitty" (= WPGMA), "median" (= WPGMC) or "centroid" (= UPGMC).
 #'@return Displays a heatmap in the current plot window.
 #'@examples
-#'BCLH(your_data = zh33, names = colnames(zh33), n_clones = 10,
+#'BCheatmap(your_data = zh33, names = colnames(zh33), n_clones = 10,
 #'       your_title = "First Time Point", grid = TRUE, columnLabels = 3)
-#'BCLH(your_data = zh33, n_clones = 10, printtable = TRUE)
+#'BCheatmap(your_data = zh33, n_clones = 10, printtable = TRUE)
 #'@export
 
 
 BCheatmap <- function(your_data, names = colnames(your_data), n_clones = 10,
                       your_title = "", grid = TRUE, columnLabels = 1, dendro = "none",
-                      star_size = 1, printtable = FALSE, log_transform = TRUE, log_choice = exp(1),
+                      star_size = 1, printtable = FALSE,
+                      table_option = "percents", log_transform = TRUE, log_choice = exp(1),
                       distance_method = "euclidean", minkowski_power = 1,
                       cellnote_option = "stars", hclust_linkage = "complete") {
 
-  #scales all data to be a percentage of reads instead of number of reads
+  #scales all data to be a percentage of reads instead of number of reads and keeps copy of raw read number
+  raw_reads <- your_data
   your_data <- as.data.frame(prop.table(as.matrix(your_data),2))
   your_data[is.na(your_data)] <- 0
 
@@ -42,53 +46,44 @@ BCheatmap <- function(your_data, names = colnames(your_data), n_clones = 10,
   your_data_ranked <- apply(-your_data, 2, rank, ties.method = "min")
 
   #subsets those barcodes that have at least one top N clone
-  your_data <- your_data[apply(your_data_ranked, 1, function(x){any(x<=n_clones)}),]
+  top_clones_choices <- apply(your_data_ranked, 1, function(x){any(x<=n_clones)})
+  your_data <- your_data[top_clones_choices,]
+  raw_reads <- raw_reads[top_clones_choices,]
 
-  your_data_ranked <- apply(-your_data, 2, rank, ties.method = "min")
 
-  data.log <- log(your_data, log_choice)
-
-  #creates empty data frame with dimension of subsetted data
-  is_a_topclone <- data.frame(matrix(ncol = ncol(your_data), nrow = nrow(your_data)))
-
+  #creates empty data frame with dimension of subsetted data &
   #populates the empty data frame with '*' for those cells w/ top clones
+  your_data_ranked <- apply(-your_data, 2, rank, ties.method = "min")
+  is_a_topclone <- data.frame(matrix(ncol = ncol(your_data), nrow = nrow(your_data)))
   is_a_topclone[your_data_ranked <= n_clones] <- "*"
 
-  if (log_transform){
-    #takes the log of the data set
-    data.log <- log(your_data, log_choice)
+  #takes log of data
+  your_data_log <- custom_log(your_data, log_choice)
 
-    #sets the -inf values to be the minimum of the possible value in the data frame -1
-    data.log[data.log == -Inf] <- (min(data.log[data.log > -Inf]) - 1)
-
-  } else {
-    data.log = your_data
-  }
-
-  e = hclust(dist(data.log, method = distance_method), method = hclust_linkage)$order
-
-  if(grid == TRUE){
-    columnsep <- 1:ncol(data.log)
-    rowseparation <- 1:nrow(data.log)
-  } else {
-    columnsep <- NULL
-    rowseparation <- NULL
-  }
+  e = hclust(dist((if (log_transform) your_data_log else your_data), method = distance_method, p = minkowski_power),
+             method = hclust_linkage)$order
 
   if(printtable == TRUE){
-    return(your_data[e,])
+    switch(table_option,
+           reads = return(raw_reads[e,]),
+           percents = return(your_data[e,]),
+           logs = return(your_data_log[e,]))
   } else {
-    gplots::heatmap.2(as.matrix(data.log[e,]),
+    gplots::heatmap.2(as.matrix((if (log_transform) your_data_log[e,] else your_data[e,])),
                       labRow = "",
                       notecex = star_size,
                       sepwidth=c(0,0.0001),
                       sepcolor="black",
-                      colsep=columnsep,
-                      rowsep=rowseparation,
+                      colsep= if (grid) 1:ncol(your_data) else NULL,
+                      rowsep= if (grid) 1:nrow(your_data) else NULL,
                       offsetRow = -0.4,
                       offsetCol = 0,
                       Rowv = FALSE,
-                      cellnote = switch(cellnote_option, stars = is_a_topclone[e,], percents = round(your_data[e,]*100, digits = 2)),
+                      cellnote = switch(cellnote_option,
+                                        stars = is_a_topclone[e,],
+                                        reads = (raw_reads[rownames(your_data[e,]),]),
+                                        percents = t(apply(round(your_data[e,]*100, digits = 2), 1, paste0, "%")),
+                                        logs = round(your_data_log[e,], digits = 2)),
                       dendrogram = dendro,
                       notecol = "black",
                       margins = c(15,6),
@@ -101,9 +96,18 @@ BCheatmap <- function(your_data, names = colnames(your_data), n_clones = 10,
                       cexCol = columnLabels,
                       labCol = names,
                       symkey = FALSE,
+
                       key = TRUE,
                       keysize = 0.8,
                       srtCol = 45,
                       main = your_title)
   }
 }
+
+custom_log <- function(x, log_choice, inf_scaler){
+  x <- log(x, log_choice)
+  #sets the -inf values to be the minimum of the possible value in the data frame -1
+  x[x == -Inf] <- (min(x[x > -Inf]) - 1)
+  return(x)
+}
+
---
+++ R/custom_log.R
@@ -0,0 +1,6 @@
+custom_log <- function(x, log_choice, inf_scaler){
+  x <- log(x, log_choice)
+  #sets the -inf values to be the minimum of the possible value in the data frame -1
+  x[x == -Inf] <- (min(x[x > -Inf]) - 1)
+  return(x)
+}
