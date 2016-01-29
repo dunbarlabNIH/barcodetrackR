f5897be69d983f43f4e04be58b8122d19ec6e6e3
d93espinoza <d93espinoza@gmail.com>
2016-01-29 17:25:19 -0500

    BBHM

--- R/BBHM.R
+++ R/BBHM.R
@@ -3,19 +3,16 @@
 #'@description Creates a binary heatmap showing the presence of new clones from L to R in the dataset.
 #'
 #'@param your_data A data frame. Usually individual barcodes in rows and samples in columns.
-#'@param your_names Names for each column of the data frame.
 #'@param threshold A chosen threshold that decides if a barcode exists or not.
 #'@param col_labels The size of the column labels.
 #'@return Displays a binary heat map in the current plot window.
 #'@examples
-#'BBHM(your_data = zh33, names = colnames(zh33), threshold = 1000,
-#'       grid = TRUE, col_labels = 3)
-#'BBHM(your_data = zh33, grid = FALSE)
+#'BBHM(your_data = zh33, threshold = 1000, col_labels = 3)
+#'BBHM(your_data = zh33)
 #'@export
 
 
-BBHM <- function(your_data, your_names = colnames(your_data), threshold = 0, col_labels = 1){
-  your_data <- your_data[,your_names]
+BBHM <- function(your_data, threshold = 0, col_labels = 1){
   your_data <- your_data[rowSums(your_data) != 0,]
   your_data[your_data <= threshold] = 0
   your_data[your_data > threshold] = 1
