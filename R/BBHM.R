63efad0337b9f8c3b58077aa21cd6744d09db217
d93espinoza <d93espinoza@gmail.com>
2016-01-30 19:28:22 -0500

    attempt #4

--- R/BBHM.R
+++ R/BBHM.R
@@ -16,7 +16,10 @@ BBHM <- function(your_data, threshold = 0, col_labels = 1){
   print(head(your_data))
   your_data[your_data <= threshold] <- 0
   your_data[your_data > threshold] <- 1
+  print(head(your_data))
   your_data <- your_data[rowSums(your_data) != 0,]
+  print("D")
+  print(head(your_data))
   if(any(colSums(your_data) == 0)){
     stop("BBHM: One of your columns had no reads above the threshold.")
   }
