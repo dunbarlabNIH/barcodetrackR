53120140b9a33181278fa769ca23f4b1fe3d3892
d93espinoza <d93espinoza@gmail.com>
2016-02-16 18:30:42 -0500

    updating ternary plot

--- R/ternary_plot.R
+++ R/ternary_plot.R
@@ -1,5 +1,5 @@
 ternary_plot <- function(your_data, colors = "random", dot_size = 2000, scale_abundance = TRUE,
-                         show_numeric_scale = FALSE, corner_labels = 100, show_arrows = TRUE,
+                         show_numeric_scale = FALSE, corner_labels = 20, show_arrows = TRUE,
                          show_breaks = TRUE, density_mode = FALSE){
   if (ncol(your_data) != 3){
     stop("You must include 3 columns of data.")
@@ -18,23 +18,27 @@ ternary_plot <- function(your_data, colors = "random", dot_size = 2000, scale_ab
   colnames(your_data) <- c("X1", "X2", "X3")
 
   if(density_mode == TRUE){
-    point_size = 1
+    point_size = .5
     point_color = "black"
   } else {
     point_size = ABUNDANCE * dot_size
     point_color = COLORS
   }
 
-  ggtern::ggtern(your_data, ggtern::aes(X1, X2, X3))+
 
-    ggtern::stat_density_tern(geom='polygon', ggtern::aes(fill=..level..),base="identity", colour = 'white') +
-    ggplot2::scale_fill_gradient(low='green',high='red')+
+  g <- ggtern::ggtern(your_data, ggtern::aes(X1, X2, X3))+
     ggplot2::geom_point(size = point_size, fill = point_color, shape = 21)+
-    ggtern::theme_bw()+ ggtern::theme_nogrid()+
-    ggplot2::labs(x = label_names[1], y = label_names[2], z = label_names[3], size = corner_labels)+
-    ggtern::scale_T_continuous(if (show_breaks) {breaks = NULL} else {waiver()})+
-    ggtern::scale_L_continuous(if (show_breaks) {breaks = NULL} else {waiver()})+
-    ggtern::scale_R_continuous(if (show_breaks) {breaks = NULL} else {waiver()})+
-    if(show_arrows) ggtern::theme_arrowlong()
+    ggplot2::labs(x = label_names[1], y = label_names[2], z = label_names[3])
+  if (density_mode == TRUE) {
+    g <- g + ggtern::stat_density_tern(geom='polygon', ggtern::aes(fill=..level..), base="identity", colour = 'black') +
+      ggplot2::scale_fill_gradient(low='green',high='red', guide = FALSE) + ggtern::theme_bw(base_size = corner_labels) + ggtern::theme_nogrid()
+  } else {
+    g <- g + ggplot2::theme_minimal(base_size = corner_labels)
+  }
+  if (show_arrows == TRUE)
+    g <- g + ggtern::theme_arrowlong()
+
+  g+ggtern::theme_hidelabels()+ggtern::theme_hideticks()
+
 
 }
