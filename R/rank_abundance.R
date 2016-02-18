38abe54a4db5643defe2d72df27868567ae21e9b
d93espinoza <d93espinoza@gmail.com>
2016-02-18 16:49:38 -0500

    Fixed rank_abundance to normalize ranks to percentages.

--- R/rank_abundance.R
+++ R/rank_abundance.R
@@ -11,23 +11,26 @@
 #'rank_abundance_plot(your_data = zh33_Tlineage[,c(1,2,4)], dot_size = 4)
 #'@export
 rank_abundance_plot <- function(your_data, dot_size = 5, text_size = 10){
+  data_list <- list()
   your_data <- your_data[rowSums(your_data) != 0,]
-  your_data <- 100*prop.table(as.matrix(your_data), margin = 2)
+  your_data <- data.frame(100*prop.table(as.matrix(your_data), margin = 2))
   for(i in 1:ncol(your_data)){
-    your_data[,i] <- your_data[order(-your_data[,i]),i]
-    your_data[,i] <- cumsum(your_data[,i])
+    data_list[[i]] <- your_data[,i,drop = FALSE]
+    data_list[[i]] <- data_list[[i]][data_list[[i]][,1] > 0,,drop = FALSE]
+    data_list[[i]]$Rank <- 100 * 1/nrow(data_list[[i]])
+    data_list[[i]] <- data_list[[i]][order(-data_list[[i]][,1]),]
+    data_list[[i]][,1] <- cumsum(data_list[[i]][,1])
+    data_list[[i]][,2] <- cumsum(data_list[[i]][,2])
+    data_list[[i]][,3] <- colnames(data_list[[i]])[1]
+    rownames(data_list[[i]]) <- NULL
+    colnames(data_list[[i]]) <- c("Prop", "Rank", "Sample")
   }
-  your_data <- reshape2::melt(your_data)
-  colnames(your_data) <- c("Barcode", "Sample", "Prop")
-  your_data$Rank <- 0
-  samples <- unique(your_data$Sample)
-  for(x in samples){
-    your_data[your_data$Sample == x,]$Rank <- rank(your_data[your_data$Sample == x,]$Prop, ties.method = "min")
-  }
+  your_data <- do.call(rbind, data_list)
+
   ggplot2::ggplot(data = your_data, ggplot2::aes(x = Rank, y = Prop, group = Sample))+
     ggplot2::geom_point(ggplot2::aes(fill = Sample, colour = Sample), shape = 21, size = dot_size)+
     ggplot2::theme_bw()+
-    ggplot2::scale_x_continuous(breaks = seq(0,max(your_data$Rank),by = 200))+
+    ggplot2::scale_x_continuous(breaks = seq(0,max(your_data$Rank),by = 10))+
     ggplot2::scale_y_continuous(breaks = seq(0,100,by = 10))+
     ggplot2::theme(text = ggplot2::element_text(size = text_size))
 }
