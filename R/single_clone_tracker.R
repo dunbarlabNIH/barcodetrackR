e6d518934d7942e3b3cf8303cdc82e461726d19f
d93espinoza <d93espinoza@gmail.com>
2016-07-20 12:01:53 -0400

    tracking top 10 instead now

--- R/single_clone_tracker.R
+++ R/single_clone_tracker.R
@@ -12,18 +12,24 @@
 #'single_clone_tracker(datum[,c("T 1m", "T 3m" "T 5m")], y_max = 0.1, top_value = 0.05)
 #'@export
 single_clone_tracker <- function(your_data, n_clones = 100, y_max = 1, line_size = 2, top_value = .5, text_size = 15) {
+  myPalette <- c("#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", 
+                 "#CBD588", "#5F7FC7", "#673770", "#D3D93E", "#38333E", "#508578", 
+                 "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD", "#D14285", "#6DDE88", 
+                 "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D", 
+                 "#8A7C64", "#599861")
+  colnames(your_data) <- 1:ncol(your_data)
   your_data <- as.data.frame(prop.table(as.matrix(your_data), margin = 2))
   your_data <- your_data[barcodetrackR::gettopindices(your_data, top = n_clones),]
-  colnames(your_data) <- 1:ncol(your_data)
-  your_data$TOTAL <- rowSums(your_data)
-  your_data$TOTAL <- apply(your_data,1,max)
-  your_data <- your_data[order(your_data$TOTAL),]
-  your_data$GROUP <- as.factor(1:nrow(your_data))
-  your_data <- reshape2::melt(your_data, id.vars = c("TOTAL", "GROUP"))
-  ggplot2::ggplot(your_data, ggplot2::aes(x = variable, y = value, color = TOTAL, group = GROUP))+
-    ggplot2::geom_line(size = line_size, lineend = 'round')+
+  your_data <- your_data[order(-apply(your_data,1,max)),]
+  your_data$BARC <- as.factor(rev(1:nrow(your_data)))
+  your_data$COLOOR <- c(RColorBrewer::brewer.pal(8, 'Set1'), "cyan", "black", rep('grey', nrow(your_data)-10))
+  your_data <- reshape2::melt(your_data, id.vars = c("BARC", "COLOOR"))
+  print(head(your_data, n = 100))
+  print(str(your_data))
+  ggplot2::ggplot(your_data, ggplot2::aes(x = variable, y = value))+
+    ggplot2::geom_line(ggplot2::aes(group = BARC, color = COLOOR), size = line_size, lineend = 'round')+
     ggplot2::scale_y_continuous(labels = function(x){paste(round(x*100, 2), "%")})+
-    ggplot2::scale_color_gradientn(colors = colorRampPalette(colors = c("grey", "red", "red"))(100), name = "Abundance\n", values = c(0,top_value,1), limits = c(0,1), labels = function(x){paste(x*100, "%")})+
+    ggplot2::scale_color_identity()+
     ggplot2::coord_cartesian(ylim = c(0,y_max))+
     ggplot2::ylab("\nAbundance\n")+
     ggplot2::xlab("\nTimepoint\n")+
