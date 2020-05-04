c26000ae250efd336fff314d82020c203ed7fa35
ryland-mortlock <rylandmortlock@gmail.com>
2020-05-04 16:05:56 -0400

    aesthetic improvements to ui

--- inst/barcode_app/ui.R
+++ inst/barcode_app/ui.R
@@ -3,24 +3,34 @@ shinyUI(
   fluidPage(
     titlePanel("Barcode App"),
     a(href="http://github.com/d93espinoza/barcodetrackR", "Link to GitHub repository (source code, sample data, etc.)"),
-    h5(""),
+    hr(),
     fluidRow(
-      column(12,
-             wellPanel(
-               div(style="display:inline-block; width:40%;",fileInput('file1', '1. Upload data (rows as observations, columns as samples)')),
-               div(style="display:inline-block; width:1%;", h5("")),
-               div(style="display:inline-block; width:40%;",fileInput('file2', '2. Upload metadata (must have SAMPLENAME column matching columns of data)')),
-               div(style="display:inline-block; width:1%;", h5("")),
-              # div(style="display:inline-block; width:15%;",fileInput('file3', '3. Upload readme (optional)')),
-              # div(style="display:inline-block; width:1%;", h5("")),
-               div(style="display:inline-block; width:15%;",numericInput("thresholdvalue", "4. Set threshold", value = 0.0000)), div(style="display:inline-block; width:1%;", h5("")),
-               div(style="display:inline-block; width:1%;", h5("")),
-               div(style="display:inline-block; width:15%;",uiOutput("thresholdPanel")),
-               div(style="display:inline-block; width:1%;", h5("")),
-               div(style="display:inline-block; width:15%;", uiOutput("thresholdInfo")),
-               style = "padding: 10px;"
-             )
-      )
+      column(4,
+             h4("1. Upload data"),
+             div(style="display:inline-block; width:100%;",fileInput('file1', 'Tabular data with rows as observations, columns as samples'))
+      ),
+      column(4,
+             h4("2. Upload metadata"),
+             div(style="display:inline-block; width:100%;",fileInput('file2', 'Tabular metadata with SAMPLENAME column matching columns of data'))
+      ),
+      column(4,
+             h4("3. Set threshold"),
+             div(style="display:inline-block; width:100%;",numericInput("thresholdvalue", "3. Set threshold", value = 0.0000)),
+             div(style="display:inline-block; width:100%;",uiOutput("thresholdPanel")),
+             div(style="display:inline-block; width:100%;", uiOutput("thresholdInfo"))
+      ),
+      #  div(style="display:inline-block; width:1%;", h5("")),
+      #  div(style="display:inline-block; width:40%;",fileInput('file2', '2. Upload metadata (must have SAMPLENAME column matching columns of data)')),
+      #  div(style="display:inline-block; width:1%;", h5("")),
+      # # div(style="display:inline-block; width:15%;",fileInput('file3', '3. Upload readme (optional)')),
+      # # div(style="display:inline-block; width:1%;", h5("")),
+      #  div(style="display:inline-block; width:15%;",numericInput("thresholdvalue", "3. Set threshold", value = 0.0000)), #, div(style="display:inline-block; width:1%;", h5("")),
+      #  div(style="display:inline-block; width:1%;", h5("")),
+      #  div(style="display:inline-block; width:15%;",uiOutput("thresholdPanel")),
+      #  div(style="display:inline-block; width:10%;", h5("")),
+      #  div(style="display:inline-block; width:15%;", uiOutput("thresholdInfo")),
+      # style = "padding: 10px;"
+      
     ),
     uiOutput("Panel")
   )
