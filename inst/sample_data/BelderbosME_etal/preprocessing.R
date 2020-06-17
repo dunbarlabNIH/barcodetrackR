7dbaecfafa7ddd973d683cd5e446ccf8da8cf545
Diego Espinoza <d93espinoza@users.noreply.github.com>
2020-06-17 18:13:10 -0400

    fix belderbos II

--- inst/sample_data/BelderbosME_etal/preprocessing.R
+++ inst/sample_data/BelderbosME_etal/preprocessing.R
@@ -4,8 +4,13 @@ require(dplyr)
 require(magrittr)
 
 raw_belderbos_data <- readxl::read_excel("inst/sample_data/BelderbosME_etal/mmc2.xlsx", sheet = "C21")
-colnames(raw_belderbos_data) <- gsub("BM", "BM_", colnames(raw_belderbos_data))
+colnames(raw_belderbos_data) <- gsub("BM", "sac_BM_", colnames(raw_belderbos_data))
+colnames(raw_belderbos_data) <- gsub("Spleen", "sac_Spleen", colnames(raw_belderbos_data))
+colnames(raw_belderbos_data) <- gsub("Liver", "sac_Liver", colnames(raw_belderbos_data))
 raw_belderbos_data <- raw_belderbos_data[,!grepl("_sum", colnames(raw_belderbos_data))]
+colnames(raw_belderbos_data) <- gsub("unsorted", "U", colnames(raw_belderbos_data))
+colnames(raw_belderbos_data)[c(2:4, 29, 30)] <- paste0(colnames(raw_belderbos_data)[c(2:4, 29,30)], "_U")
+
 raw_belderbos_data <- as.data.frame(raw_belderbos_data)
 rownames(raw_belderbos_data) <- raw_belderbos_data$ID
 raw_belderbos_data$ID <- NULL
@@ -17,7 +22,10 @@ belderbos_metadata <- data.frame(SAMPLENAME = colnames(raw_belderbos_data),
                                               "B", "T", "B", "T", "G", "bulk",
                                               "B", "T", "G", "bulk", "B", "T", "G",
                                               "bulk", "B", "bulk"),
-                                 organ = c(rep("PB", 6), rep("BM", 16), "Spleen"))
+                                 organ = c(rep("PB", 6), rep("BM_Front", 3),
+                                           rep("BM_Left", 3), rep("BM_Right", 4),
+                                           rep("BM_Spine", 4), rep("BM_Pelvis",2), "Spleen"))
+belderbos_metadata$SAMPLENAME <- ifelse(belderbos_metadata$weeks == "sac", paste0("sac_", belderbos_metadata$SAMPLENAME), belderbos_metadata$SAMPLENAME)
 
 write.table(file = "inst/sample_data/BelderbosME_etal/count_matrix_mouse_C21.txt", raw_belderbos_data, sep = '\t', quote = FALSE)
 write.table(file = "inst/sample_data/BelderbosME_etal/metadata_mouse_C21.txt", belderbos_metadata, sep = '\t', quote = FALSE)
