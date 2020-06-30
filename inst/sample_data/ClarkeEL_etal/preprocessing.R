e14b10dd349e973368264b180a514c27cc1c69db
ryland-mortlock <rylandmortlock@gmail.com>
2020-06-29 21:25:08 -0400

    clarke et al tcr data

--- inst/sample_data/ClarkeEL_etal/preprocessing.R
+++ inst/sample_data/ClarkeEL_etal/preprocessing.R
@@ -1,5 +1,39 @@
 #download the raw data from https://doi.org/10.5281/zenodo.1256169 which is the data repository for https://genomemedicine.biomedcentral.com/articles/10.1186/s13073-018-0580-z
 
+## T Cell Receptor (TCR) Sequencing Data
+load('your_file_path/clarke_tcr_data.rds') # Downloaded from zenodo link above
+# The TCR data is too large to include in the Github repository, it can be downloaded from the link above and saved locally. 
+
+# Counts data
+tcr[["seqs"]] %>%
+  mutate(identifier = paste(accn, replicate, sep = "_")) %>%
+  tidyr::pivot_wider(id_cols = nucleotide, values_from = count, names_from = identifier) %>%
+  as.data.frame -> clarke_TCR_reads
+
+rownames(clarke_TCR_reads) <- clarke_TCR_reads$nucleotide
+clarke_TCR_reads$nucleotide <- NULL
+clarke_TCR_reads[is.na(clarke_TCR_reads)] <- 0
+
+# Metadata keeping only patient 7
+tcr[["mdata"]] %>%
+  dplyr::filter(patient == "SCID00007") %>%
+  mutate(SAMPLENAME = paste(accn, replicate, sep = "_")) %>%
+  arrange(nmonths, replicate) %>%
+  select(SAMPLENAME, everything()) -> clarke_TCR_metadata_p7
+
+# Keep just patient 7 data in read counts
+clarke_TCR_reads_p7 <- clarke_TCR_reads[,colnames(clarke_TCR_reads) %in% clarke_TCR_metadata_p7$SAMPLENAME]
+clarke_TCR_reads_p7 <- clarke_TCR_reads_p7[rowSums(clarke_TCR_reads_p7)>0,]
+
+# Match order from metadata
+clarke_TCR_reads_p7 <- clarke_TCR_reads_p7[,match(colnames(clarke_TCR_reads_p7),clarke_TCR_metadata_p7$SAMPLENAME)]
+
+# Write to files
+write.table(file = "inst/sample_data/ClarkeEL_etal/tcr_reads_p00007.txt", clarke_TCR_reads_p7, sep = '\t', quote = FALSE)
+write.table(file = "inst/sample_data/ClarkeEL_etal/tcr_metadata_p00007.txt", clarke_TCR_metadata_p7, sep = '\t', quote =  FALSE)
+
+
+## Viral Integration Site (VIS) Data
 raw_clarke_data <- read.csv("inst/sample_data/ClarkeEL_etal/intSiteData.csv")
 
 raw_clarke_data %>%
