70ec3a8d4b405c9eddcc1eafe8a9adef2e501d48
ryland-mortlock <rylandmortlock@gmail.com>
2020-07-22 18:59:02 -0400

    one more

--- R/circos_plot.R
+++ R/circos_plot.R
@@ -51,7 +51,7 @@ temp_prop <- temp_prop[rowSums(temp_binary)>1,]
 temp_binary <- temp_binary[rowSums(temp_binary)>1,]
 
 # Count the number of occurences of the unique combinations
-unique_count <- as.data.frame(temp_binary) %>% dplyr::group_by_all %>% matrixStats::count
+unique_count <- as.data.frame(temp_binary) %>% dplyr::group_by_all() %>% matrixStats::count()
 # Sort decreasing
 unique_count <- unique_count[do.call(order,-unique_count),]
 
