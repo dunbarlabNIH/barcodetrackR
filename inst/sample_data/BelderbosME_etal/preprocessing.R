2968ebf6d16606f18d83e6bcca8945e0ab578915
ryland-mortlock <rylandmortlock@gmail.com>
2020-10-31 17:39:46 -0400

    update Belderbos et al sample data

--- inst/sample_data/BelderbosME_etal/preprocessing.R
+++ inst/sample_data/BelderbosME_etal/preprocessing.R
@@ -1,9 +1,9 @@
-#download the raw data from https://www.bbmt.org/article/S1083-8791(19)30566-X/abstract
+#download the raw data from https://www.bbmt.org/article/S1083-8791(19)30566-X/fulltext#supplementaryMaterial
 
 require(dplyr)
 require(magrittr)
 
-raw_belderbos_data <- readxl::read_excel("inst/sample_data/BelderbosME_etal/mmc2.xlsx", sheet = "C21")
+raw_belderbos_data <- readxl::read_excel("inst/sample_data/BelderbosME_etal/belderbosME_raw_data.xlsx", sheet = "C21")
 colnames(raw_belderbos_data) <- gsub("BM", "sac_BM_", colnames(raw_belderbos_data))
 colnames(raw_belderbos_data) <- gsub("Spleen", "sac_Spleen", colnames(raw_belderbos_data))
 colnames(raw_belderbos_data) <- gsub("Liver", "sac_Liver", colnames(raw_belderbos_data))
@@ -26,6 +26,9 @@ belderbos_metadata <- data.frame(SAMPLENAME = colnames(raw_belderbos_data),
                                            rep("BM_Left", 3), rep("BM_Right", 4),
                                            rep("BM_Spine", 4), rep("BM_Pelvis",2), "Spleen"))
 
-write.table(file = "inst/sample_data/BelderbosME_etal/count_matrix_mouse_C21.txt", raw_belderbos_data, sep = '\t', quote = FALSE)
-write.table(file = "inst/sample_data/BelderbosME_etal/metadata_mouse_C21.txt", belderbos_metadata, sep = '\t', quote = FALSE)
+write.table(file = "inst/sample_data/BelderbosME_etal/count_matrix_mouse_C21.txt", raw_belderbos_data,
+            sep = '\t', quote = FALSE, row.names = TRUE, col.names = NA)
+
+write.table(file = "inst/sample_data/BelderbosME_etal/metadata_mouse_C21.txt", belderbos_metadata,
+            sep = '\t', quote = FALSE, row.names = FALSE)
 
