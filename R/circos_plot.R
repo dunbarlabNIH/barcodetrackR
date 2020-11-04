9a158c50332cf7463430718f5081544cdf02b685
ryland-mortlock <rylandmortlock@gmail.com>
2020-11-03 23:03:39 -0500

    try again

--- R/circos_plot.R
+++ R/circos_plot.R
@@ -12,7 +12,7 @@
 #'@return Displays a chord diagram in the current plot window.
 #'
 #'@import viridis
-#'@importFrom dplyr group_by_all
+#'@import dplyr
 #'
 #'@export
 #'
@@ -50,7 +50,7 @@ temp_prop <- temp_prop[rowSums(temp_binary)>1,]
 temp_binary <- temp_binary[rowSums(temp_binary)>1,]
 
 # Count the number of occurences of the unique combinations
-unique_count <- as.data.frame(temp_binary) %>% dplyr::group_by_all %>% count
+unique_count <- as.data.frame(temp_binary) %>% group_by_all %>% count
 # Sort decreasing
 unique_count <- unique_count[do.call(order,-unique_count),]
 
