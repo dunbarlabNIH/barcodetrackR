3e2124366ce6eed8cd444d56d20fd2b04d436aa4
Ryland Mortlock <rylandmortlock@gmail.com>
2020-11-19 09:32:23 -0500

    add vegan dependency

--- R/clonal_diversity.R
+++ R/clonal_diversity.R
@@ -7,7 +7,7 @@
 #'@param group_by_choices Choice(s) from the column designated in group_by that will be used for plotting. Defaults to all if left as NULL.
 #'@param plot_over The column of metadata that you want to be the x-axis of the plot. e.g. timepoint
 #'@param plot_over_display_choices Choice(s) from the column designated in plot_over that will be used for plotting. Defaults to all if left as NULL.
-#'@param index_type Character. One of "shannon", "shannon_count", "simpson", or "invsimpson.
+#'@param index_type Character. One of "shannon", "shannon_count", "simpson", or "invsimpson. 
 #'@param point_size Numeric. Size of points.
 #'@param line_size Numeric. Size of lines.
 #'@param text_size Numeric. Size of text in plot.
@@ -16,6 +16,8 @@
 #'
 #'@return Outputs plot of a diversity measure tracked for groups over a factor. Or if return_table is set to true, a dataframe will be returned instead.
 #'
+# #'@importFrom diverse diversity 
+#'@importFrom vegan diversity
 #'@importFrom rlang %||%
 #'@importFrom magrittr %>%
 #'@import tibble
@@ -81,6 +83,16 @@ clonal_diversity <- function(your_SE,
   } else {
     stop("index_type must be one of \"shannon\", \"shannon_count\", \"simpson\", or \"invsimpson\"")
   }
+  
+  # #calculate measure for each sample
+  # if(index_type %in% c("variety", "entropy","blau","gini-simpson", "simpson", "hill-numbers", "herfindahl-hirschman", "bergerparker", "renyi", "evenness", "rao", "rao-stirling")){
+  #   diverse::diversity(as.matrix(your_data), type = index_type, category_row = TRUE) %>%
+  #     rownames_to_column(var = "SAMPLENAME") %>%
+  #     dplyr::rename(index = all_of(index_type)) %>%
+  #     dplyr::mutate(index_type = index_type) -> calculated_index
+  # } else {
+  #   stop("index_type must be one of \"variety\", \"entropy\", \"blau\", \"gini-simpson\", \"simpson\", \"hill-numbers\", \"herfindahl-hirschman\", \"bergerparker\", \"renyi\", \"evenness\", \"rao\", or \"rao-stirling\"")
+  # }
 
   # merge measures with colData
   temp_subset_coldata %>%
