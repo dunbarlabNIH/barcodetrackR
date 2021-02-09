68e89d804aab348b6467cbf5ebd4310ab4de7d40
Ryland Mortlock <rylandmortlock@gmail.com>
2021-02-09 18:24:54 -0500

    function to estimate barcode thresholds

--- R/create_SE.R
+++ R/create_SE.R
@@ -4,7 +4,8 @@
 #'
 #'@param your_data A data frame. For clonal tracking data, this will be individual barcodes or lineage tracing elements in rows and samples in columns.
 #'@param meta_data A data frame containing all meta-data. Must, at the very least, include a column called "SAMPLENAME" that contains all of the colnames within the data frame passed as  `your_data` and only those colnames.
-#'@param threshold Optional. A numeric indicating which reads to filter out (only those rows which have higher than `threshold` proportion of reads within at least one sample will be kept as non-zero) prior to creating the SE object.
+#'@param threshold Numeric. The minimum threshold abundance for a barcode to be maintained in the SE. If `threshold_type` is relative, this parameter should be between 0 and 1. If `threshold_type` is absolute, this parameter should be greater than 1. 
+#'@param threshold_type Character. One of "relative" or "absolute" relative. If a relative threshold is specified, only those rows which have higher than `threshold` proportion of reads within at least one sample will be kept as non-zero. If an absolute threshold is specified, only those rows which have an absolute read count higher than `threshold` in at least one sample will be kept as non-zero. 
 #'@param log_base A numeric indicating which base to use when logging the percentages.
 #'@param scale_factor A numeric indicating what scaling factor to use in normalization. For the default value of 1 million, barcode percentages on a per sample basis will be multiplied by 1 million before log+1 normalization.
 #'
@@ -20,20 +21,60 @@
 create_SE <- function(your_data = NULL,
                       meta_data = NULL,
                       threshold = 0,
+                      threshold_type = "relative",
                       log_base = exp(1),
                       scale_factor = 1e6){
+  
+  # Error checking
   if(is.null(your_data) | is.null(meta_data)){
     stop("NEITHER `your_data` NOR `meta_data` can be NULL")
   }
   if(!setequal(colnames(your_data), meta_data$SAMPLENAME) | ncol(your_data) != nrow(meta_data)){
     stop("The SAMPLENAME column in meta_data must only contain ALL columns in your_data")
   }
-  if(threshold > 0){
-    your_data <- threshold(your_data, thresh = threshold)
+  
+  if (threshold_type %in% c("relative","absolute") == FALSE){
+    stop("The parameter `threshold_type` must be set to `relative` or `absolute`.")
   }
+  
+  if (threshold_type == "relative"){
+    if (threshold < 0 | threshold >= 1){
+      stop("Since `threshold_type` is set to `relative`, `threshold` must be greater than or equal to 0 and less than 1.")
+    }
+  }
+  
+  if (threshold_type == "absolute"){
+    if (threshold <= 1){
+      stop("Since `threshold_type` is set to `absolute`, `threshold` must be greater than 1.")
+    }
+  }
+  
+  # Thresholding
+  pre_thresh_bc_num <- nrow(your_data[rowSums(your_data)>0,])
+  if (threshold == 0){
+    cat("No threshold supplied. All barcodes will be retained. Be aware that lower abundance barcodes are likely to be less reliable due to sampling bias. To estimate an appropriate threshold, please see the barcodetrackR function `estimate_barcode_threshold`. \n")
+  }
+  else if(threshold != 0){
+    your_data <- threshold(your_data, thresh = threshold, thresh_type = threshold_type)
+    cat("Removed", pre_thresh_bc_num - nrow(your_data), "barcodes from the supplied dataframe based on", threshold_type, "threshold of", threshold, "\n")
+  }
+  
+  # Check that all samples contain data
   if(any(colSums(your_data) == 0)){
-    stop("One of your samples has no data after thresholding")
+    bad_samples <- which(colSums(your_data) == 0)
+    if (threshold == 0){
+      cat("The following samples have no data. \n")
+      cat(colnames(your_data)[bad_samples], sep = ", ")
+      cat("\n")
+      stop("Please remove the empty sample before instantiating the SE.")
+    } else if (threshold != 0){
+      cat("The following samples have no data after thresholding. \n")
+      cat(colnames(your_data)[bad_samples], sep = ", ")
+      at("\n")
+      stop("Please try a more permissive threshold or remove the sample(s) prior to instantiating the SE.")
+    }
   }
+  
   your_data.ranks <-  as.data.frame(apply(-your_data, 2, rank, ties.method = "min", na.last = "keep"))
   your_data.percentages <-  as.data.frame(prop.table(as.matrix(your_data),2))
   your_data.normalized <- your_data.percentages * scale_factor
@@ -47,8 +88,14 @@ create_SE <- function(your_data = NULL,
 
   S4Vectors::metadata(your_SE)$scale_factor <- scale_factor
   S4Vectors::metadata(your_SE)$log_base <- log_base
-
-
+  if (threshold == 0){
+    S4Vectors::metadata(your_SE)$threshold_type <- "none"
+    S4Vectors::metadata(your_SE)$threshold_value <- threshold
+  } else if (threshold != 0){
+    S4Vectors::metadata(your_SE)$threshold_type <- threshold_type
+    S4Vectors::metadata(your_SE)$threshold_value <- threshold
+  }
+  
   return(your_SE)
 }
 
---
+++ R/estimate_barcode_threshold.R
@@ -0,0 +1,73 @@
+#'@title Estimate Barcode Threshold
+#'
+#'@description Estimates an appropriate minimum abundance threshold for reliably detected barcodes in a clonal tracking dataset. \cr \cr 
+#'For a specified capture efficiency C, the minimum clone size N that we can expect to detect with confidence level P is calculated from: \cr `P = 1 - (1 - C)^(N)` \cr \cr 
+#'The proportional abundance of a clonal tag of size N is \cr `N / (T * F)` \cr where T is the total population size of cells or genomes and F is the frequency or proportion of the total population which is labeled or genetically modified with the clonal tag. \cr \cr 
+#'The population size and proportion labeled must be determined experimentally. The capture efficiency should be estimated for a given clonal tracking technique by simulating the barcode retrieval process in silico and finding the capture efficiency which leads to a total # of detected barcodes matching the experimentally determined number. Adair et al `(PMID: 32355868)` performed this analysis for viral integration site analysis and DNA barcode sequencing and determined good estimates for the capture efficiencies of these two technologies to be 0.05 and 0.4 respectively. 
+#'
+#'@param your_SE A Summarized Experiment object.
+#'@param capture_efficiency Numeric. The capture efficiency of the clonal tracking method to detect a given clone. Must be between 0 and 1. See the description for details on how to estimate this value for a given experiment. A reasonable value for viral integration site analysis is 0.05 and for DNA barcode sequencing is 0.4 - see Adair, et al 2020. PMID: 32355868.
+#'@param population_size Numeric. The total number of cells/genomes within each sample analyzed in the clonal tracking study. This is an experimentally determined value.
+#'@param proportion_labeled Numeric. The proportion of the population which is expected to be genetically modified or contain a clonal tracking index. This is an experimentally determined value.
+#'@param confidence_level Numeric. The confidence level for estimatig the minimum abundance threshold. Must be between 0 and 1. Default is 0.95 for 95 percent confidence that a clone with proportion `relative_threshold` will be detected. Increasing this parameter closer to one will result in a more stringent abundance threshold and decreasing this parameter will result in a more permissive abundance threshold.
+#'@param verbose Logical. Whether to print the calculated thresholds.  
+#'
+#'@return Returns a list with the element `relative_threshold` describing the proportional abundance above which clones can be considered reliable and the element `absolute_threshold` describing the absolute count above which clones can be considered reliable. Pass either of these values into the function `threshold_SE` to threshold an existing SummarizedExperiment object or the function `create_SE` to threshold a SummarizedExperiment object upon creation from dataframes of counts and metadata.
+#'
+#'@import SummarizedExperiment
+#'@importFrom rlang %||%
+#'
+#'@examples
+#'estimate_barcode_threshold(your_SE = wu_subset, capture_efficiency = 0.4, population_size = 500000, proportion_labeled = 0.3, confidence_level = 0.95, verbose = TRUE)
+#'
+#'@export
+#'
+estimate_barcode_threshold <- function(your_SE,
+                                       capture_efficiency = NULL,
+                                       population_size = NULL,
+                                       proportion_labeled = NULL,
+                                       confidence_level = 0.95,
+                                       verbose = TRUE){
+  # Error checking
+  if (capture_efficiency){
+    if (capture_efficiency <= 0){
+      stop("The `capture_efficiency` argument must be greater than 0.")
+    } else if (capture_efficiency >= 1){
+      stop("The `capture_efficiency` argument must be less than 1.")
+    } 
+  } else if (is.null(capture_efficiency)){
+    cat("No capture efficiency provided. Using 0.05 as default. The capture efficiency depends on the clonal tracking technology and should be estimated for each experiment. \n")
+  }
+  capture_efficiency <- capture_efficiency %||% 0.05
+  
+  if (is.null(population_size)){
+    stop("The `population_size` argument must be specified. This is an experimentally determined parameter.")
+  }
+  
+  if (is.null(proportion_labeled)){
+    stop("The `proportion_labeled` argument must be specified. This is an experimentally determined parameter.")
+  }
+  
+  if (confidence_level <= 0){
+    stop("The `confidence_level` argument must be greater than 0.")
+  } else if (confidence_level >= 1){
+    stop("The `confidence_level` argument must be less than 1.")
+  } else if (confidence_level != 0.95){
+    cat("Non-default confidence level specified:",confidence_level,"\n")
+  }
+  
+  # Perform calculation
+  output_list <- list()
+  output_list[["relative_threshold"]] <- log((1 - confidence_level), base = (1 - capture_efficiency)) / (population_size * proportion_labeled)
+  output_list[["absolute_threshold"]] <- log((1 - confidence_level), base = (1 - capture_efficiency)) * capture_efficiency
+  
+  # Print results if desired
+  if (verbose){
+    cat("Relative threshold. Barcodes above", output_list[["relative_threshold"]]*100, "% of a given sample are estimated to be reliable. \n")
+    cat("Absolute threshold. Barcodes with counts above", round(output_list[["absolute_threshold"]], 2), "are estimated to be reliable.")
+  }
+  
+  return(output_list)
+
+}
+
