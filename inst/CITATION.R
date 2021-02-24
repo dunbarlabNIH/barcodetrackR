b8085c481f39ef561dcf43d660be18f805e87180
Diego Espinoza <diegoespinoza@Diegos-MacBook-Pro.local>
2021-02-24 01:06:09 -0500

    add CITATIOn

---
+++ inst/CITATION.R
@@ -0,0 +1,13 @@
+citEntry(entry = "article",
+         author = personList(as.person("Diego A. Espinoza"),
+                             as.person("Ryland D. Mortlock"),
+                             as.person("Samson J. Koelle"),
+                             as.person("Chuanfeng Wu"),
+                             as.person("Cynthia E. Dunbar")),
+         title = "barcodetrackR: an R package for the interrogation of clonal tracking data",
+         journal = "bioRxiv",
+         year = "2020",
+         doi = "10.1101/2020.07.23.212787",
+         url = "https://doi.org/10.1101/2020.07.23.212787",
+         textVersion = "Espinoza and Mortlock et al. barcodetrackR: an R package for the interrogation of clonal tracking data. bioRxiv (2020)"
+)
