17ac69bfe36910249edbf5ab676adb6cd41594eb
Diego Espinoza <diegoespinoza@Diegos-MacBook-Pro.local>
2021-02-24 02:26:49 -0500

    fix launchapp example

--- R/launchApp.R
+++ R/launchApp.R
@@ -5,9 +5,7 @@
 #' @param x NULL
 #' @return Page launching the Shiny Barcode App
 #' @examples
-#' \donttest{
 #' if (interactive()) launchApp()
-#' }
 #' @export
 launchApp <- function(x = NULL) {
     shiny::runApp(system.file("barcode_app", package = "barcodetrackR"))
